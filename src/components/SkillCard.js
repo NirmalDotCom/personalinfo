e2051901b2ae4b71141877504bfffa9cddfc71c7
Nirmal <96161923+NirmalDotCom@users.noreply.github.com>
2024-07-19 17:44:01 +0530

    CgCPlusPlus

--- src/components/SkillCard.js
+++ src/components/SkillCard.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import "./SkillCardStyle.css";
-import {SiPhp} from "react-icons/cg";
+import { CgCPlusPlus } from "react-icons/cg";
 import {DiGit} from "react-icons/di";
-import {SiVisualstudiocode, SiLaravel, SiWordpress, SiBootstrap, SiFigma, SiAdobephotoshop} from "react-icons/si";
+import {SiVisualstudiocode, SiLaravel, SiWordpress, SiBootstrap, SiFigma, SiAdobephotoshop } from "react-icons/si";
 import {TbBrandCss3, TbBrandJavascript, TbBrandHtml5} from "react-icons/tb";
 import {IoLogoWindows, SiMysql} from "react-icons/io"
 
@@ -14,7 +14,7 @@ export default function SkillCard({id}) {
           <div className="skill-box">
             <h2>Languages</h2>
             <div className="skillset">
-              <abbr title='PHP'><SiPhp className='techLogo'/></abbr>
+              <abbr title='PHP'><CgCPlusPlus className='techLogo'/></abbr>
               <abbr title='HTML 5'><TbBrandHtml5 className='techLogo'/></abbr>
               <abbr title='JavaScript'><TbBrandJavascript className='techLogo'/></abbr>
             </div>                     
