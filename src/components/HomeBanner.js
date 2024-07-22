5a92613edbdbdf48712575319e402917baed0e00
Nirmal <96161923+NirmalDotCom@users.noreply.github.com>
2024-07-22 16:47:16 +0530

    output

--- src/components/HomeBanner.js
+++ src/components/HomeBanner.js
@@ -2,53 +2,22 @@ import React from 'react';
 import "./HomeBannerStyle.css";
 import cartoon from "../images/cartoon-compressed.png";
 
-const darkTheme = "dark-theme";
-
-// Add dark theme change here
-const darkThemeSetUp = () => {
-  if (getCurrentTheme() === "dark") {
-    document.getElementById("toggleBtn").checked = true;
-  } else {
-    document.getElementById("toggleBtn").checked = false;
-  }
-};
-
-const getCurrentTheme = () =>
-  document.body.classList.contains(darkTheme) ? "dark" : "light";
-
-//   Get user's theme preference from local storage
-const selectedTheme = localStorage.getItem("selected-theme");
-if (selectedTheme === "dark") {
-  document.body.classList[selectedTheme === "dark" ? "add" : "remove"](
-    darkTheme
-  );
-  darkThemeSetUp();
-}
-
-const themeButton = document.getElementById('toggleBtn');
-if (el) {
-  el.addEventListener('click', swapper, false);
-}
-
-window.addEventListener("change", (event) => {
-  const el = document.getElementById('toggleBtn');
-  if (el) {
-    el.addEventListener('click', swapper, false);
-  }
-});
-
-
 export default function HomeBanner({id}) {
   return (
     <div className="home" id={id}> 
         <div className='content'>
             <div className="wrapper">
-          <input type="checkbox" class="checkbox" id="toggleBtn" />
-          <label class="switch" for="toggleBtn">
-            <i class="fas fa-moon"></i>
-            <i class="fas fa-sun"></i>
-            <div class="ball"></div>
-          </label>
+            <div class="slide-toggle">
+  <input id="guidLater"
+         type="checkbox" checked
+         onchange="toggleTheme(this.checked)"/>
+  <label for="guidLater">
+    <div><i class="fas fa-sun"></i></div>
+    <div><i class="fas fa-moon"></i></div>
+  </label>
+</div>
+
+<div id="output" style="margin-top: 1rem"></div>
               <div className="name">Nirmal Raj Kirubakaran</div>
               <div className="staticTitle">
                 Software
