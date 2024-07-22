492a6b6f1028cc98fe3798a8bed519c177733b37
Nirmal <96161923+NirmalDotCom@users.noreply.github.com>
2024-07-22 17:36:34 +0530

    support

--- src/components/HomeBanner.js
+++ src/components/HomeBanner.js
@@ -2,11 +2,26 @@ import React from 'react';
 import "./HomeBannerStyle.css";
 import cartoon from "../images/cartoon-compressed.png";
 
+const checkbox = document.getElementById("checkbox")
+checkbox.addEventListener("change", () => {
+  document.body.classList.toggle("dark")
+})
+
 export default function HomeBanner({id}) {
   return (
     <div className="home" id={id}> 
         <div className='content'>
             <div className="wrapper">
+            <h1>Light/Dark Toggle<br> Button</h1>
+
+<div>
+  <input type="checkbox" class="checkbox" id="checkbox" />
+  <label for="checkbox" class="checkbox-label">
+    <i class="fas fa-moon"></i>
+    <i class="fas fa-sun"></i>
+    <span class="ball"></span>
+  </label>
+</div>
               <div className="name">Nirmal Raj Kirubakaran</div>
               <div className="staticTitle">
                 Software
