6e823f9b54f6592bbc3a68fceecdb4f69bf06783
Nirmal <96161923+NirmalDotCom@users.noreply.github.com>
2024-07-22 17:19:41 +0530

    querySelectorAll

--- src/components/HomeBanner.js
+++ src/components/HomeBanner.js
@@ -2,6 +2,35 @@ import React from 'react';
 import "./HomeBannerStyle.css";
 import cartoon from "../images/cartoon-compressed.png";
 
+// If CSS Custom Properties or Variables are supported
+if (window.CSS && CSS.supports("color", "var(--primary)")) {
+	const radios = document.querySelectorAll('input[type="radio"]');
+
+	const toggleColorMode = function toggleColorMode(e) {
+		//console.log(e.currentTarget.value);
+		// Switch to Light Mode
+		if (e.currentTarget.value === "off") {
+			// Sets the custom html attribute
+			document.documentElement.setAttribute("color-mode", "dark");
+			// Sets the user's preference in local storage
+			localStorage.setItem("color-mode", "dark");
+			return;
+		}
+		/* Switch to Light Mode */
+		document.documentElement.setAttribute("color-mode", "light");
+		localStorage.setItem("color-mode", "light");
+	};
+
+	radios.forEach(function (radio) {
+		radio.addEventListener("click", toggleColorMode);
+	});
+} else {
+	//hide the switcher
+	const switcherContainer = document.querySelectorAll(".head-widgets")[0];
+	switcherContainer.style.display = "none";
+}
+
+
 export default function HomeBanner({id}) {
   return (
     <div className="home" id={id}> 
