5f1fb49989360ab086147486c7880ed437d7c8ab
Nirmal <96161923+NirmalDotCom@users.noreply.github.com>
2024-07-22 18:12:12 +0530

    buttonEl

--- src/components/HomeBanner.js
+++ src/components/HomeBanner.js
@@ -27,8 +27,6 @@ function updateButton({ buttonEl, isDark }) {
   const newCta = isDark ? "Change to light theme" : "Change to dark theme";
   // use an aria-label if you are omitting text on the button
   // and using a sun/moon icon, for example
-  buttonEl.setAttribute("aria-label", newCta);
-  buttonEl.innerText = newCta;
 }
 
 /**
