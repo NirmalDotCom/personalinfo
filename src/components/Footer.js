aedd105bc1de3c17da304f51d7d113e30d94466a
Nirmal <96161923+NirmalDotCom@users.noreply.github.com>
2024-07-23 17:22:08 +0530

    Profile

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import "./FooterStyle.css";
+import myProfile from "../images/profile-image2.jpg";
 
 export default function Footer() {
   const styleSymbol = {
@@ -13,7 +14,7 @@ export default function Footer() {
     <div>
    
         <div className='footer'>
-        <img className='bg' src="./images/projectRogfree.png" alt="" />
+        <img className="img" src={myProfile} alt="Profile"></img>
         Made with ❤ by Nirmal <span style={styleSymbol}>©</span> {year}
         </div>
     </div>
