13e774c8a662c436a2a07b86601d83e9fcc8d58d
Nirmal <96161923+NirmalDotCom@users.noreply.github.com>
2024-07-23 14:35:30 +0530

    test

--- src/components/HomeBanner.js
+++ src/components/HomeBanner.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import "./HomeBannerStyle.css";
 import cartoon from "../images/cartoon-compressed.png";
-const setTheme = theme => document.documentElement.className = theme;
+
 export default function HomeBanner({id}) {
   return (
     <div className="home" id={id}> 
@@ -12,14 +12,6 @@ export default function HomeBanner({id}) {
                 Software
                 <div className='hrLine'></div>
               </div>
-              <div class="toggle-container">
-    <button class="theme-btn light" onclick="setTheme('light')" title="Light mode">
-      <img src="https://assets.codepen.io/210284/sun.png" alt="sun" />
-    </button>
-    <button class="theme-btn dark" onclick="setTheme('dark')" title="Dark mode">
-      <img src="https://assets.codepen.io/210284/moon.png" alt="moon" />
-    </button>
-  </div>
               <ul className="dynamicTitle">
                 <li>+<span>Developer</span></li>
               </ul>
