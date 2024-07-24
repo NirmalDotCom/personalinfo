66a25512574809add4d77b21b4a89df147de71c6
Nirmal <96161923+NirmalDotCom@users.noreply.github.com>
2024-07-24 17:38:52 +0530

    BrowserRouter

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import store from './store';
-import { Provider } from 'react-redux'
 import {BrowserRouter} from "react-router-dom"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
