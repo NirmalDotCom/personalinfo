216ec1f62f5cf2a14a59da0ea3581f1cbf0c0539
Nirmal <96161923+NirmalDotCom@users.noreply.github.com>
2024-07-24 15:45:42 +0530

    useSelector

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,53 +1,29 @@
-import React from 'react';
-import { playAudio, stopAudio, selectIsPlaying } from "./audioSlice.js";
-import { useDispatch, useSelector } from "react-redux";
-import "./FooterStyle.css";
-import imgdvd from "../images/dvdplayer.png";
-import testaud from "../images/ringplayer.mp3";
+import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { decrement, increment } from './counterSlice'
+import styles from './Counter.module.css'
 
-export default function Footer() {
-  const styleSymbol = {
-    fontSize: 20,
-    marginRight: 7,
-    marginLeft: 7
-  }
+export function Counter() {
+  const count = useSelector((state) => state.counter.value)
+  const dispatch = useDispatch()
 
-  const dispatch = useDispatch();
-  const isPlaying = useSelector(selectIsPlaying);
-
-  const stopAnimation = () => {
-    const stop = setTimeout(() => {
-      dispatch(stopAudio());
-      console.log('dispatch: ', 'dispatch');
-    }, 184000)
-
-    return () => clearTimeout(stop)
-  }
-
-  const handleButtonClick = () => {
-    if (isPlaying) {
-      dispatch(stopAudio());
-      console.log('stopAudio: ', 'stopAudio');
-    } else {
-      dispatch(playAudio());
-      console.log('playAudio: ', 'playAudio');
-    }
-
-    stopAnimation()
-  };
-
-  const today = new Date();
-  const year = today.getFullYear();
   return (
     <div>
-   
-        <div className='footer'>
-        <img src={imgdvd} className={`nirmal-img ${isPlaying && "rotate-disk"}`} onClick={handleButtonClick} alt='Vinyl' />
-      {isPlaying && <audio src={testaud} autoPlay={isPlaying} />}
-        Made with ❤ by Nirmal <span style={styleSymbol}>©</span> {year} 
-        </div>
+      <div>
+        <button
+          aria-label="Increment value"
+          onClick={() => dispatch(increment())}
+        >
+          Increment
+        </button>
+        <span>{count}</span>
+        <button
+          aria-label="Decrement value"
+          onClick={() => dispatch(decrement())}
+        >
+          Decrement
+        </button>
+      </div>
     </div>
-
-     
   )
 }
