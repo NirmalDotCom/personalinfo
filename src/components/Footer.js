03a8cd47180291212e21708adf73d367c3cb6d11
Nirmal <96161923+NirmalDotCom@users.noreply.github.com>
2024-07-24 12:08:02 +0530

    isPlaying

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,9 @@
 import React from 'react';
+import { playAudio, stopAudio, selectIsPlaying } from "./audioSlice.js";
+import { useDispatch, useSelector } from "react-redux";
 import "./FooterStyle.css";
+import imgdvd from "../images/dvdplayer.png";
+import testaud from "../images/ringplayer.mp3";
 
 export default function Footer() {
   const styleSymbol = {
@@ -8,12 +12,42 @@ export default function Footer() {
     marginLeft: 7
   }
 
+  const dispatch = useDispatch();
+  const isPlaying = useSelector(selectIsPlaying);
+
+  const stopAnimation = () => {
+    const stop = setTimeout(() => {
+      dispatch(stopAudio());
+      console.log('dispatch: ', 'dispatch');
+    }, 184000)
+
+    return () => clearTimeout(stop)
+  }
+
+  const handleButtonClick = () => {
+    if (isPlaying) {
+      dispatch(stopAudio());
+      console.log('stopAudio: ', 'stopAudio');
+    } else {
+      dispatch(playAudio());
+      console.log('playAudio: ', 'playAudio');
+    }
+
+    stopAnimation()
+  };
+
   const today = new Date();
   const year = today.getFullYear();
-
   return (
-    <div className='footer'>
+    <div>
+   
+        <div className='footer'>
+        <img src={imgdvd} className={`nirmal-img ${isPlaying && "rotate-disk"}`} onClick={handleButtonClick} alt='Vinyl' />
+      {isPlaying && <audio src={testaud} autoPlay={isPlaying} />}
         Made with ❤ by Nirmal <span style={styleSymbol}>©</span> {year} 
+        </div>
     </div>
+
+     
   )
 }
