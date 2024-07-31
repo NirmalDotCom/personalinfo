069156e3c99ce9951593a04f64055600c2c58eba
Nirmal <96161923+NirmalDotCom@users.noreply.github.com>
2024-07-31 11:53:41 +0530

    year

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,6 @@
-import React, { useRef, useState } from 'react';
+import React from 'react';
 import "./FooterStyle.css";
 import AudioPlayer from './AudioPlayer';
-import './AudioPlayerStyle.css'; // Optional: For custom styles
 
 export default function Footer() {
   const styleSymbol = {
@@ -13,30 +12,11 @@ export default function Footer() {
   const today = new Date();
   const year = today.getFullYear();
 
-  const [isPlaying, setIsPlaying] = useState(false);
-  const audioRef = useRef(null);
-
-  const togglePlayPause = () => {
-    if (isPlaying) {
-      audioRef.current.pause();
-    } else {
-      audioRef.current.play();
-    }
-    setIsPlaying(!isPlaying);
-  };
-
-  
-
 
   return (
-    <div>
-    <span>Made with 💙 by Nirmal</span>
-    <div className="audio-player">
-      <button onClick={togglePlayPause}>
-        {isPlaying ? 'Pause' : 'Play'}
-      </button>
+    <div className='footer'>
+        Made with💙by Nirmal <span style={styleSymbol}>©</span> {year}. 
     </div>
-  </div>
   )
 }
 
